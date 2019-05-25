1b5657f4d77f721a728c8ea81076c0b0144c9586
Bruno Agutoli <bruno.agutoli@gmail.com>
2019-05-25 15:29:42 +1000

    bugfix/issue-8

--- src/AbstractService.js
+++ src/AbstractService.js
@@ -7,8 +7,14 @@ class AbstractService {
     this.stackName = plugin.getStackName();
     this.bucketName = plugin.getBucketName();
     this.provider = this.plugin.provider;
+
     this.packageJsonKeyName = path.join(plugin.getBucketLayersPath(), 'package.json');
     this.zipFileKeyName = `${path.join(this.plugin.getBucketLayersPath(), this.stackName)}.zip`;
+
+    if (/^win/.test(process.platform)) {
+      this.zipFileKeyName = this.zipFileKeyName.replace(/\\/g, '/');
+      this.packageJsonKeyName = this.packageJsonKeyName.replace(/\\/g, '/');
+    }
   }
 }
 
