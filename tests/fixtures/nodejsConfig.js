cd7563a1d319409c8265ff253a789f8ec4454c4f
Bruno Agutoli <bruno.agutoli@rotorstudios.com>
2020-04-18 22:50:12 +1000

    fix test

--- tests/fixtures/nodejsConfig.js
+++ tests/fixtures/nodejsConfig.js
@@ -2,6 +2,7 @@ module.exports = {
   runtime: 'nodejs12.x',
   runtimeDir: 'nodejs',
   packageManager: 'yarn',
+  libraryFolder: 'node_modules',
   dependenciesPath: './fixtures/package.json',
   compatibleRuntimes: [ 'nodejs' ],
   copyBeforeInstall: [ 'yarn.lock', 'package-lock.json' ],
