738fe3c7808da7e1786a0c2bae457401c4d7361d
Bruno Agutoli <bruno.agutoli@rotorstudios.com>
2021-06-19 17:57:31 +1000

    fixing demo

--- demos/simple/handler.py
+++ demos/simple/handler.py
@@ -1,12 +1,7 @@
-import numpy as np
-
+import requests
 
 def main(event, context):
-    a = np.arange(15).reshape(3, 5)
-
-    print("Your numpy array:")
-    print(a)
-
+    print(requests)
 
 if __name__ == "__main__":
     main('', '')
